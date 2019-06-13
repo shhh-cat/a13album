2c101eb0bedeed9e68d23e19f74191bf9a29cf3b
thien-nguyen <laotienboi2804@gmail.com>
2019-06-13 19:27:17 +0700

    change button login

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,23 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 text-center">
-            <h1 class="m-3">Đăng nhập</h1>
-            <a href="{{ route('facebook.login')}}" class="btn btn-social btn-facebook font-weight-bold">
-                 <i class="fab fa-facebook-square"></i> Đăng nhập bằng Facebook
-            </a>
-            {{-- <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div> --}}
+            @guest
+                <h1 class="m-3">Đăng nhập</h1>
+                <a href="{{ route('facebook.login')}}" class="btn btn-social btn-facebook font-weight-bold">
+                        <i class="fab fa-facebook-square"></i> Đăng nhập bằng Facebook
+                </a>
+            @else
+                <h1 class="m-3">Đăng nhập</h1>
+                <a href="{{ route('facebook.login')}}" class="btn btn-social btn-facebook font-weight-bold">
+                        <i class="fab fa-facebook-square"></i> Tiếp tục với {{ Auth::user()->name }} <img src="{{ Auth::user()->avatar }}" alt="Facebook Avatar" height="28" width="28" class="ml-2 rounded">
+                </a>
+            @endguest
         </div>
     </div>
 </div>
