69c1f652f5d8775a4c16a4c345bd5aae3c9af9fe
thien-nguyen <laotienboi2804@gmail.com>
2019-06-17 19:47:20 +0700

    remove height main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-    <a class="col-md-6 text-center btn text-dark" href="{{ route('getupload')}}" style="height: 50vh;">
+    <a class="col-md-6 text-center btn text-dark" href="{{ route('getupload')}}"
             <h1>Lưu trữ ảnh</h1>
             <i class="fas fa-cloud-upload-alt fa-10x"></i>
         </a>
-        <a class="col-md-6 text-center btn text-dark" href="{{ route('album')}}" style="height: 50vh;">
-            <h1>Xem ảnh công khai</h1>
-            <i class="fas fa-images fa-10x"></i>
-        </a>
+    <a class="col-md-6 text-center btn text-dark" href="{{ route('album')}}">
+        <h1>Xem ảnh công khai</h1>
+        <i class="fas fa-images fa-10x"></i>
+    </a>
     </div>
 </div>
 @endsection
