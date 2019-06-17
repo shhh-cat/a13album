695a7606575951de98e4e07bb7da49061c293658
thien-nguyen <laotienboi2804@gmail.com>
2019-06-17 19:57:13 +0700

    fix '>' h2 tag main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-    <a class="col-md-6 text-center btn text-dark" href="{{ route('getupload')}}"
+    <a class="col-md-6 text-center btn text-dark" href="{{ route('getupload')}}">
             <h1>Lưu trữ ảnh</h1>
             <i class="fas fa-cloud-upload-alt fa-10x"></i>
         </a>
