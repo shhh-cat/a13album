52eadc9ddc1cb3918d3105d94762dc5fda7ec482
thien-nguyen <laotienboi2804@gmail.com>
2019-06-18 12:55:54 +0700

    fix something

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,7 +11,7 @@
         <h1>Xem ảnh công khai</h1>
         <i class="fas fa-images fa-10x"></i>
     </a>
-    <a class="col-md-4 text-center btn text-dark" href="{{ route('album')}}">
+    <a class="col-md-4 text-center btn text-dark" href="{{ route('manager')}}">
             <h1>Quản lí ảnh</h1>
             <i class="fas fa-tasks fa-10x"></i>
         </a>
